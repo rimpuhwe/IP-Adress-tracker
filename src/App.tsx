119c9a3239847a848962f16e33720356532f279e
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2025-01-26 12:54:45 +0200

    removing the fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import IpTracker from "./IpTracker";
 import "./App.css";
 
-const API_KEY = "60f288361458402fbb118f785afa57c7";
-const API_URL = "https://api.ipgeolocation.io/ipgeo";
-
-export const getLocation = async (ip: string) => {
-  try {
-    const response = await fetch(`${API_URL}?apiKey=${API_KEY}&ip=${ip}`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching location:", error);
-    return null;
-  }
-};
-
 function App() {
   return (
     <>
-      <div className='border border-solid border-transparent bg-[url("/pattern-bg-desktop.png")]'>
+      <div className="border border-solid border-transparent bg-[url('/pattern-bg-desktop.png')]">
         <IpTracker />
       </div>
     </>
